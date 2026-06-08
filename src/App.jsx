f142236d0551d1ef12e6c7a564ad4fae1e56252b
guesswhojustme <imnayeonsaranghaee@gmail.com>
2026-06-08 13:03:47 +0800

    add practical exp card component

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,14 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
-import { GeneralInfoCard, EducationExpCard } from './components/Cards'
+import { GeneralInfoCard, EducationExpCard, PracticalExpCard } from './components/Cards'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
      <>
-     <EducationExpCard></EducationExpCard>
+     <PracticalExpCard></PracticalExpCard>
      </>
   )
 }
--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -45,3 +45,35 @@ export function EducationExpCard(){
         </div>
     )
 }
+
+export function PracticalExpCard(){
+    return (
+        <div className='pe-container'>
+            <span className='card-title'>Practical Experience</span>
+            <div className='first-part'>
+                <div>
+                    <span>company name:</span>
+                    <input type="text" name="company-name" id="company-name" placeholder='company name'/>
+                </div>
+                <div>
+                    <span>position title:</span>
+                    <input type="text" name="position-title" id="position-title" placeholder='whateveritis'/>
+                </div>
+            </div>
+            <div className='second-part'>
+                    <span>work responsibility: </span>
+                    <textarea placeholder='what did you do during your work?'></textarea>
+            </div>
+            <div className='third-part'>
+                <div>
+                    <span>date start:</span>
+                    <input type="text" placeholder='mm/yy'/>
+                </div>
+                <div>
+                    <span>date end:</span>
+                    <input type="text" placeholder='mm/yy'/>
+                </div>
+            </div>
+        </div>
+    )
+}
