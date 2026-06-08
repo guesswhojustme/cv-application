24680af70c096a16b6fd12f3c80c2fe96929d6ce
guesswhojustme <imnayeonsaranghaee@gmail.com>
2026-06-08 12:36:09 +0800

    add education exp card component

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,14 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
-import { GeneralInfoCard } from './components/Cards'
+import { GeneralInfoCard, EducationExpCard } from './components/Cards'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
      <>
-     <GeneralInfoCard/>
+     <EducationExpCard></EducationExpCard>
      </>
   )
 }
--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -2,7 +2,7 @@ import './card.css';
 
 export function GeneralInfoCard(){
     return (
-        <div className='container'>
+        <div className='gi-container'>
            <span className='card-title'>General Information</span>
            <div className='name-wrapper'>
             <span>name:</span>
@@ -15,6 +15,32 @@ export function GeneralInfoCard(){
            <div className='email-wrapper'>
             <span>email:</span>
              <input type="email" name="email" id="email" placeholder='whateverisyour@email.com'/>
+           </div>
+        </div>
+    )
+}
+
+export function EducationExpCard(){
+    return (
+        <div className='ee-container'>
+           <span className='card-title'>Educational Experience</span>
+           <div>
+            <span>school name:</span>
+            <input type="text" name="name" id="name" placeholder='school name'/>
+           </div>
+           <div>
+            <span>title of study:</span>
+            <input type="text" name="phone" id="phone" placeholder='0987654321'/>
+           </div>
+           <div className='date-wrapper'>
+            <div>
+                <span>date start:</span>
+                <input type="text" placeholder='mm/yy'/>
+            </div>
+            <div>
+                <span>date end:</span>
+                <input type="text" placeholder='mm/yy'/>
+            </div>
            </div>
         </div>
     )
